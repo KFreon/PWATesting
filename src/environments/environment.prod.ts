29938b5bc2539626ef5542745e2c08d99aa3188e
Kael Larkin <kael.larkin@readify.net>
2018-06-14 14:30:27 +1000

    Auto-generated commit

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    aad: {
-      tenantId: '0881953f-106e-4372-8440-72db98be0d2e',
-      appId: '50bf251b-31b5-4797-9fcd-f8462b21af52'
-    }
-  };
